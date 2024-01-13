0411b5efe7f7f3fbf7355bfc1da61e85f9066f66
virpx <vierypangeran@gmail.com>
2024-01-13 20:40:28 +0700

    Update getPPTeacher.js

--- controller/getPPTeacher.js
+++ controller/getPPTeacher.js
@@ -73,6 +73,15 @@ const getPPTeacher = async (req, res) => {
     Bucket: 'cyclic-amused-kerchief-eel-eu-west-3',
     Key: lokasinya,
   };
+  let fileData = '';
+  s3Stream.on('data', (chunk) => {
+    // Mengumpulkan setiap bagian data file
+    fileData += chunk;
+  });
+  s3Stream.on('end', () => {
+    const base64Data = Buffer.from(fileData).toString('base64');
+    res.status(200).send(base64Data);
+  });
   const s3Stream = s3.getObject(params).createReadStream();
   s3Stream.pipe(res);
   // return res.status(200).sendFile(lokasinya, { root: "." });
