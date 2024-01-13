ead95bfe9b4745421d3d4d71afe6db5d1168d659
virpx <vierypangeran@gmail.com>
2024-01-13 21:36:13 +0700

    Update getPPTeacher.js

--- controller/getPPTeacher.js
+++ controller/getPPTeacher.js
@@ -74,7 +74,7 @@ const getPPTeacher = async (req, res) => {
     Key: lokasinya,
   };
   const s3Stream = s3.getObject(params).createReadStream();
-  res.setHeader('content-type', 'image/png');
+  res.setHeader('content-type', 'image/'+lokasinya.split('.')[1]);
   s3Stream.pipe(res);
   // return res.status(200).sendFile(lokasinya, { root: "." });
 };
